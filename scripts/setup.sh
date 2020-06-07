f317a0d961615e6558436d7e861c2884b3b0f8cf
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2020-06-07 14:16:57 -0700

    Create Basic CI Flow (#6)
    Travis CI clones the repo, builds the docker image, and pushes it to GCR. Next steps will be to upload helm chart and deploy to k8s.
    
    * Create .gitignore
    
    * Add basic Flask skeleton
    
    * Dockerfile runs with gunicorn on port 8080
    
    * Helm chart created and tested on GKE
    
    * Specify ingress controller
    
    * Guicorn runs on PORT env variable, adjust helm chart
    
    * Guicorn runs on PORT env variable
    
    * Add index for helm repo
    
    * Added index file to wrong repo
    
    * Modify for static IP address
    
    * Lower worker count because cluster was too expensive
    
    * Add README
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Increment chart and app version
    
    * Enable HPA
    
    * Attempt to build and ship docker image using Travis CI
    
    * Attempt 2 for Travis
    
    * Attempt 3 for Travis
    
    * Fix creds file on CI
    
    * Skip user prompts on CI
    
    * Fix line breaks
    
    * Remove steps
    
    * Clean up CI files
    
    * Fix CI files
    
    * Debug CI
    
    * Debug CI
    
    * Debug CI 3
    
    * Debug CI 4
    
    * Verified CI scripts are being cloned
    
    * Trying different approach to running ci scripts
    
    * Get steps to run sequentially
    
    * All steps have to run on a single machine
    
    * Trying to fix container upload permissions
    
    * Build on linux distro
    
    * Update service account
    
    * Debug CI again
    
    * Trying to fix permissions issue by disabling cache
    
    * Experiment with docker auth
    
    * Debugging docker auth
    
    * Readd cache and enable roles
    
    * Add project flag to service account config
    
    * Small mod to test changes in IAM
    
    * Readd google service account creds
    
    * Debug permissions for gcloud
    
    * Fix typo
    
    * Modify travis file for OS
    
    * Modify method of setting up docker config
    
    * Remove printing key on CI

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+gcloud version || true
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk;
+ curl https://sdk.cloud.google.com | bash; fi
+source /home/travis/google-cloud-sdk/path.bash.inc
+gcloud components install kubectl
+gcloud auth activate-service-account --key-file google-service-account-creds.json --project flaskhelm
+gcloud config set project flaskhelm
+gcloud container clusters get-credentials my-first-cluster-1 --zone us-central1-c --project flaskhelm
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
