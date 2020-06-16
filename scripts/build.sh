6f6b412d6dca9ec238ecdfd88aef1734bde0545f
arjungopisetty <arjun.gopisetty@gmail.com>
2020-06-16 02:49:17 -0700

    Fix helm package

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,4 @@ docker build -t flaskhelm/flask-hello-world .
 docker tag flaskhelm/flask-hello-world gcr.io/flaskhelm/flask-hello-world:${git_sha}
 docker push gcr.io/flaskhelm/flask-hello-world:${git_sha}
 
-cd flask-hello-world
-helm package flask-hello-world -d ci-cd-helm-charts
+helm package flask-hello-world -d flask-hello-world/ci-cd-helm-charts
