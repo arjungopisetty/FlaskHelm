2aef05e6ef38b505f2a8ac9a00189ccf19eac875
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2020-06-16 04:18:23 -0700

    Fix CD for Helm Repo
    * Grant higher permissions to sync script
    
    * Moving deploy on master down to script
    
    * Move deploy target up to yaml for visibility
    
    * Don't build on PR, only master

--- scripts/sync-repo.sh
+++ scripts/sync-repo.sh
@@ -65,15 +65,12 @@ main() {
 
   verify_prereqs
 
-  # dry run of the command
-  gsutil rsync -d -n $1 gs://$2
-
-  read -p "Are you sure you would like to continue with these changes? [y/N]} " confirm
-  if [[ $confirm =~ [yY](es)* ]]; then
-    gsutil rsync -d $1 gs://$2
-  else
-    error_exit "Discontinuing sync process."
-  fi
+  #read -p "Are you sure you would like to continue with these changes? [y/N]} " confirm
+  #if [[ $confirm =~ [yY](es)* ]]; then
+  gsutil rsync -d $1 gs://$2
+  #else
+    #error_exit "Discontinuing sync process."
+  #fi
 
   echo "Your remote chart repository now matches the contents of the $1 directory!"
 
