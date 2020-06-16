274bf025835393182887316b34f249a0062b3ca0
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2020-06-16 03:33:00 -0700

    Develop (#13)
    * Retry deploy step
    
    * Grant higher permissions to sync script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ docker push gcr.io/flaskhelm/flask-hello-world:master
 
 # helm local-chart-version bump -s minor -c flask-hello-world
 helm repo index ci-cd-helm-charts --url https://ci-cd-helm-charts.storage.googleapis.com/ 
-scripts/sync-repo.sh flask-hello-world/ci-cd-helm-charts ci-cd-helm-charts
+bash scripts/sync-repo.sh flask-hello-world/ci-cd-helm-charts ci-cd-helm-charts
 
 helm repo add mybucket https://ci-cd-helm-charts.storage.googleapis.com/
 helm repo list
